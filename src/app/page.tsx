80db0c0629b75af0b640f4251b70b831894925d5
Thiwanka Kaushal <thiwanka.kaushal.mob@gmail.com>
2025-12-08 13:29:56 +0530

    add Leaflet map view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+"use client";
+import dynamic from "next/dynamic";
 
-export default function Home() {
+
+export default function Page() {
+
+  const Map = dynamic(() => import("@/components/Map"), { ssr: false });
+  
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      route-elevation-engine
-    </div>
+    <main className="relative min-h-screen">
+      <Map/>
+    </main>
   );
 }
