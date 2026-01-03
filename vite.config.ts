6bdac6bce281922afc7536849033232bcf4a97c3
Thiwanka Kaushal <thiwanka.kaushal.mob@gmail.com>
2026-01-03 12:40:52 +0530

    repo cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: 'route-elevation-engine',
   plugins: [
     tailwindcss(),
   ],
